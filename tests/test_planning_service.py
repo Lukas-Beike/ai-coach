b251a032e69fcc0147c56f0c61d9a869a566fa83
Lukas Beike <lukas.beike@users.noreply.github.com>
2026-09-14 11:43:32 +0200

    refactor(planning): own plan metadata transactions

--- tests/test_planning_service.py
+++ tests/test_planning_service.py
@@ -2,10 +2,32 @@ import json
 import sqlite3
 import unittest
 
-from backend.planning.service import update_plan_bounds
+from backend.planning.service import update_plan_bounds, update_plan_metadata
 
 
 class PlanningServiceTests(unittest.TestCase):
+    def test_plan_metadata_update_owns_the_supplied_transaction(self):
+        repository = type("Repository", (), {
+            "get": lambda _self, _db, _id: {"id": _id, "name": "Base", "goal": "", "start_date": "2026-01-01", "end_date": "2026-01-03", "status": "planned"},
+            "update": lambda _self, *_args: None,
+        })()
+        committed = []
+        changed = []
+        class Transaction:
+            def __enter__(self):
+                return object()
+            def __exit__(self, *args):
+                committed.append(args[0] is None)
+        result = update_plan_metadata(
+            "p1", {"name": "Updated", "start_date": "2026-01-01", "end_date": "2026-01-04"},
+            transaction=lambda: Transaction(), repository=repository,
+            record_change=lambda *args, **kwargs: changed.append(args),
+            bump_revision=lambda _db: None, now=lambda: "2026-01-04T00:00:00+00:00",
+        )
+        self.assertEqual(result["plan"]["name"], "Updated")
+        self.assertEqual(committed, [True])
+        self.assertEqual(changed[0][3], "update")
+
     def test_plan_bounds_update_uses_caller_transaction_and_records_change(self):
         db = sqlite3.connect(":memory:")
         db.row_factory = sqlite3.Row
